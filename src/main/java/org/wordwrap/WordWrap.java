a614f6461a8213e31254102c6dc9db8dd307c8cb
aitorevi <aitorevi@gmail.com>
2023-03-30 19:23:03 +0200

    Test(red): if_original_text_is_empty_then_expect_same_text

--- src/main/java/org/wordwrap/WordWrap.java
+++ src/main/java/org/wordwrap/WordWrap.java
@@ -1,4 +1,12 @@
 package org.wordwrap;
 
 public class WordWrap {
+    public static void main(String[] args) {
+        String originalText = "";
+        TransformTextWithConsole(originalText);
+    }
+
+    public static String TransformTextWithConsole(String originalText) {
+        return "0";
+    }
 }
--- src/test/java/org/wordwrap/WordWrapTest.java
+++ src/test/java/org/wordwrap/WordWrapTest.java
@@ -1,7 +1,13 @@
 package org.wordwrap;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class WordWrapTest {
 
+    @Test
+    void if_original_text_is_empty_then_expect_same_text() {
+        assertThat(WordWrap.TransformTextWithConsole("")).isEqualTo("");
+    }
 }
