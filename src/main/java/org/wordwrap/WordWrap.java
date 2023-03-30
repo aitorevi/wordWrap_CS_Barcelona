52b848534151b7d902d5190521723f51d12fe7e9
aitorevi <aitorevi@gmail.com>
2023-03-30 19:31:55 +0200

    Test(red): if_original_text_is_the_smaller_than_the_columns

--- src/main/java/org/wordwrap/WordWrap.java
+++ src/main/java/org/wordwrap/WordWrap.java
@@ -3,10 +3,11 @@ package org.wordwrap;
 public class WordWrap {
     public static void main(String[] args) {
         String originalText = "";
-        TransformTextWithConsole(originalText);
+        int columnsNumber = 7;
+        TransformTextWithConsole(originalText,columnsNumber);
     }
 
-    public static String TransformTextWithConsole(String originalText) {
+    public static String TransformTextWithConsole(String originalText, int columnsNumber) {
         return "";
     }
 }
--- src/test/java/org/wordwrap/WordWrapTest.java
+++ src/test/java/org/wordwrap/WordWrapTest.java
@@ -8,6 +8,10 @@ class WordWrapTest {
 
     @Test
     void if_original_text_is_empty_then_expect_same_text() {
-        assertThat(WordWrap.TransformTextWithConsole("")).isEqualTo("");
+        assertThat(WordWrap.TransformTextWithConsole("",7)).isEqualTo("");
+    }
+    @Test
+    void if_original_text_is_the_smaller_than_the_columns() {
+        assertThat(WordWrap.TransformTextWithConsole("hola",7)).isEqualTo("hola");
     }
 }
