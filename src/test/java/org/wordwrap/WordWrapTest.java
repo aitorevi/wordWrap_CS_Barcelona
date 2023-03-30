e9338650ae53ba5b9fe4a334e42900723848e2c9
aitorevi <aitorevi@gmail.com>
2023-03-30 19:42:05 +0200

    Test(red): if_original_text_is_the_larger_than_the_columns

--- src/test/java/org/wordwrap/WordWrapTest.java
+++ src/test/java/org/wordwrap/WordWrapTest.java
@@ -14,4 +14,17 @@ class WordWrapTest {
     void if_original_text_is_the_smaller_than_the_columns() {
         assertThat(WordWrap.TransformTextWithConsole("hola",7)).isEqualTo("hola");
     }
+    @Test // "hola", 2 -> "ho\nla"
+    void if_original_text_is_the_larger_than_the_columns() {
+        assertThat(WordWrap.TransformTextWithConsole("hola",2)).isEqualTo("ho\nla");
+    }
 }
+// TODO:
+// "hola", 2 -> "ho\nla"
+// "ohlala", 2 -> "oh\nla\nla"
+// "hola", 3 -> "hol\na"
+// "hola", 4 -> "hola"
+// "hola mundo", 7 -> "hola\nmundo"
+// "hola", -5 -> ???
+// "", 4 -> ???
+// "hola", 0 -> ???
